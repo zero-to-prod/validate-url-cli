6640ac60033bbed8052290f47ad816728e2389bb
david_smith <david_smith@sweetwater.com>
2025-01-29 11:22:04 -0500

    refactor/to-data-models Updated `tests/Unit/SrcCommandTest.php`.

--- tests/Unit/SrcCommandTest.php
+++ tests/Unit/SrcCommandTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\Attributes\Test;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 use Tests\TestCase;
-use Zerotoprod\ValidateUrlCli\SrcCommand;
+use Zerotoprod\ValidateUrlCli\Src\SrcCommand;
 
 class SrcCommandTest extends TestCase
 {
